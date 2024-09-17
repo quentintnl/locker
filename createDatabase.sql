719d717c442366d4d395eab9af230c655f1e6745
TaupeInHambourg <129371488+TaupeInHambourg@users.noreply.github.com>
2024-09-17 14:30:17 +0200

    Update createDatabase.sql

--- createDatabase.sql
+++ createDatabase.sql
@@ -2,20 +2,10 @@ DROP TABLE IF EXISTS `Password`;
 DROP TABLE IF EXISTS `TryPassword`;
 DROP TABLE IF EXISTS `Locker`;
 
-CREATE TABLE `Password` (
-    `id` integer PRIMARY KEY AUTO_INCREMENT,
-    `pin` integer,
-    `created_at` timestamp
-);
-
 CREATE TABLE `Locker` (
     `id` integer PRIMARY KEY AUTO_INCREMENT,
-    `password_id` integer,
+    `password` integer,
     `name` varchar(255),
     `status` bool,
     `created_at` timestamp
 );
-
-ALTER TABLE `Locker` ADD FOREIGN KEY (`password_id`) REFERENCES `Password` (`id`);
-
-SELECT * FROM Password
