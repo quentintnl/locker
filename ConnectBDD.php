4e29104109b36a6518952e0d5d6973ecd844ac3c
Val-s18 <valentin.renaud188@gmail.com>
2024-09-17 14:37:22 +0200

    insert into

--- ConnectBDD.php
+++ ConnectBDD.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "locker";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
--- addLocker.sql
+++ addLocker.sql
@@ -1,5 +1,3 @@
-INSERT INTO `Locker`(`password_id`, `name`, `status`)
+INSERT INTO `Locker`(`password`, `name`, `status`)
 VALUES ('1','locker1', 0), ('2', 'locker2', 0);
 
-INSERT INTO `Password`(`pin`)
-VALUES ('1234'), ('5678');
