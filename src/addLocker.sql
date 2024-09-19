b2738465dda4b8e6728ae2c2da3157e13b4634bd
Ota <q.tonellato@free.fr>
2024-09-19 16:15:09 +0200

    feat: can create Database

--- src/addLocker.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `Raspberry`(`id`, `ip`)
-VALUES ('1','192.169.4.71'),
-       ('2','192.169.4.50');
-
-INSERT INTO `Locker`(`name`, `password`, `pin`, `close_or_open`, `ip_id`)
-VALUES  ('Locker1','$2y$10$VZgm5eA7DLgQBlzY8b0rm.EI1V7ZGLMQqA/i0WKdrujmaN7Y49tSq','17','1','1'),
-        ('Locker2','$2y$10$8ZpkYfi7blv5zxO5vk9dXOk0Iw8rLWkJHvUogG4bOCopE18.GUfGK%','27','1','2');
--- src/createDatabase.sql
+++ src/createDatabase.sql
@@ -3,17 +3,25 @@ DROP TABLE IF EXISTS `Locker`;
 DROP TABLE IF EXISTS `Ip`;
 
 CREATE TABLE Raspberry (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    ip VARCHAR(255) NOT NULL
+                           id INT PRIMARY KEY AUTO_INCREMENT,
+                           ip VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Locker (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    pin INT NOT NULL,
-    close_or_open BOOLEAN NOT NULL,
-    ip_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (ip_id) REFERENCES Raspberry(id)
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        name VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        pin INT NOT NULL,
+                        close_or_open BOOLEAN NOT NULL,
+                        ip_id INT,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        FOREIGN KEY (ip_id) REFERENCES Raspberry(id)
 );
+
+INSERT INTO `Raspberry`(`id`, `ip`)
+VALUES ('1','192.168.241.154'),
+       ('2','192.169.4.50');
+
+INSERT INTO `Locker`(`name`, `password`, `pin`, `close_or_open`, `ip_id`)
+VALUES  ('Locker1','$2y$10$VZgm5eA7DLgQBlzY8b0rm.EI1V7ZGLMQqA/i0WKdrujmaN7Y49tSq','27','1','1'),
+        ('Locker2','$2y$10$8ZpkYfi7blv5zxO5vk9dXOk0Iw8rLWkJHvUogG4bOCopE18.GUfGK','17','1','1');
